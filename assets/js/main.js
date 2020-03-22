da21541b8ed363ba9246169e0c4e4acd148d216c
family.happy89@gmail.com <flaxnguyen@gmail.com>
2020-03-22 04:49:16 -0600

    homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,35 @@ $( document ).ready(function() {
         items: 2,
         autoplay:  true,
     })
+   
 });
+(function () {
+     // Google Map
+	let map;
+	document.addEventListener("DOMContentLoaded",() => {
+		let s = document.createElement("script");
+		document.head.appendChild(s);
+		s.addEventListener("load",() => {
+      var myposition = {lat: 10.788339,  lng: 106.702597};
+			map = new google.maps.Map(document.getElementById('map'), {
+			center: myposition,
+			zoom: 20,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+			});
+      var contentString = '<div class="content_map"><h3>18Bis/12 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, TP. HCM</h3><a href="#">Get Direction</a></div>';
+       var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+
+      var marker = new google.maps.Marker({
+        position: myposition,
+        map: map,
+        title: 'TheoDore'
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+		});
+		s.src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCQQ_JBNIzJv_au-RLszU12TYMmNN36LZM';
+	});
+}());
