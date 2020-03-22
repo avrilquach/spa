9bf0e9e0f0674173cd24d793f6ef3b0d8c8162c6
family.happy89@gmail.com <flaxnguyen@gmail.com>
2020-03-21 22:11:20 -0600

    homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
 $( document ).ready(function() {
-    $('.owl-carousel').owlCarousel({
+    $('.slider').owlCarousel({
         loop: true,
         dots: true,
         items: 1,
         autoplay:  true,
+    })
+    $('.gallery').owlCarousel({
+        loop: true,
+        nav: true,
+        margin: 15,
+        items: 3,
+        autoplay:  true,
+
     })
 });
