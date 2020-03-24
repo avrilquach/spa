f6fea348f49c72520fd65ed1f37d03649e19111a
tuanna6@runsystem.net <tuanna6@runsystem.net>
2020-03-24 16:20:52 +0700

    dichvu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $('.slider').owlCarousel({
         loop: true,
         dots: true,
         items: 1,
-        autoplay:  true,
+        autoplay: true,
     })
     $('.gallery').owlCarousel({
         loop: true,
@@ -11,16 +11,16 @@ $( document ).ready(function() {
         dots: false,
         margin: 15,
         items: 3,
-        autoplay:  true,
-        responsive:{
-            0:{
-                items:1
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            991:{
-                items:2
+            991: {
+                items: 2
             },
-            1200:{
-                items:3
+            1200: {
+                items: 3
             }
         }
     })
@@ -29,45 +29,45 @@ $( document ).ready(function() {
         dots: true,
         margin: 50,
         items: 2,
-        autoplay:  true,
-        responsive:{
-            0:{
-                items:1
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            1200:{
-                items:2
+            1200: {
+                items: 2
             }
         }
     })
     AOS.init();
 });
 (function () {
-     // Google Map
-	let map;
-	document.addEventListener("DOMContentLoaded",() => {
-		let s = document.createElement("script");
-		document.head.appendChild(s);
-		s.addEventListener("load",() => {
-      var myposition = {lat: 10.788339,  lng: 106.702597};
-			map = new google.maps.Map(document.getElementById('map'), {
-			center: myposition,
-			zoom: 20,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-			});
-      var contentString = '<div class="content_map"><h3>18Bis/12 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, TP. HCM</h3><a href="#">Get Direction</a></div>';
-       var infowindow = new google.maps.InfoWindow({
-        content: contentString
-      });
+    // Google Map
+    let map;
+    document.addEventListener("DOMContentLoaded", () => {
+        let s = document.createElement("script");
+        document.head.appendChild(s);
+        s.addEventListener("load", () => {
+            var myposition = { lat: 10.788339, lng: 106.702597 };
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: myposition,
+                zoom: 20,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            var contentString = '<div class="content_map"><h3>18Bis/12 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, TP. HCM</h3><a href="#">Get Direction</a></div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
 
-      var marker = new google.maps.Marker({
-        position: myposition,
-        map: map,
-        title: 'TheoDore'
-      });
-      marker.addListener('click', function() {
-        infowindow.open(map, marker);
-      });
-		});
-		s.src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCWaD7ncidnSTPFdFssKuSK7uSJQrPPINo';
-	});
+            var marker = new google.maps.Marker({
+                position: myposition,
+                map: map,
+                title: 'TheoDore'
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+        });
+        s.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCWaD7ncidnSTPFdFssKuSK7uSJQrPPINo';
+    });
 }());
